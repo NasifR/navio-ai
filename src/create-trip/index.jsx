5d69ae93a4df6090afca05c3af0904fc1099790c
unknown <nasifjuhayer@gmail.com>
2025-08-20 15:45:32 -0400

    trip info form

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -5,7 +5,12 @@ function CreateTrip() {
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
       <h2 className='font-bold text-3xl'>Your trip, but make it ✨personal✨</h2>
       <p className='mt-3 text-gray-700 text-xl'>Think of this as ordering off a secret travel menu. Give us the basics, and our AI will serve up a plan sprinkled with surprises you’ll actually love. (We promise!😉)</p>
-
+      
+      <div className='mt-10'>
+        <div>
+          <h2 className='text-xl my-3 font-medium'>What's your destination</h2>
+        </div>
+      </div>
     </div>
   )
 }
