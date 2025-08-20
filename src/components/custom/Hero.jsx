5a0b0afef2dd5df7a2baff5d0c9e3c90f7b45cc8
unknown <nasifjuhayer@gmail.com>
2025-08-19 23:42:08 -0400

    hero section

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Button } from '../ui/button'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div>
+      <h1 className="font-extrabold text-[50px]  text-center">
+        <span className='text-[#17a4b0]'>Plan smarter, travel farther</span><br></br>
+        <span className='text-black'>Your AI-powered trip planner for seamless adventures.</span>
+      </h1>
+      <Button className='mt-10'>Get Started</Button>
+    </div>
   )
 }
 
