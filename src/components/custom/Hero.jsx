7e2b179935168b09da88f70e9f00c9bb90eb52ae
unknown <nasifjuhayer@gmail.com>
2025-08-20 15:12:19 -0400

    create trip header

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Button } from '../ui/button'
-
+import { Link } from 'react-router-dom'
 function Hero() {
   return (
     <div>
@@ -8,7 +8,9 @@ function Hero() {
         <span className='text-[#17a4b0]'>Plan smarter, travel farther</span><br></br>
         <span className='text-black'>Your AI-powered trip planner for seamless adventures.</span>
       </h1>
+      <Link to={'/create-trip'}>
       <Button className='mt-10'>Get Started</Button>
+      </Link>
     </div>
   )
 }
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function CreateTrip() {
   return (
-    <div>CreateTrip</div>
+    <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
+      <h2 className='font-bold text-3xl'>Your trip, but make it ✨personal✨</h2>
+      <p className='mt-3 text-gray-700 text-xl'>Think of this as ordering off a secret travel menu. Give us the basics, and our AI will serve up a plan sprinkled with surprises you’ll actually love. (We promise!😉)</p>
+
+    </div>
   )
 }
 
