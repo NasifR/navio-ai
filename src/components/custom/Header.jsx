f2702569ac9f09612dc1bd11d04f8a309437eb65
unknown <nasifjuhayer@gmail.com>
2025-08-19 21:18:51 -0400

    added logo

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Button } from '../ui/button'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className="w-full p-3 shadow-sm flex justify-between items-center px-5 fixed top-0 left-0 bg-white z-50">
+      <img src="/navio_logo.png" alt="Navio Logo" className="h-10" />
+      <Button>Sign in</Button>
+    </div>
   )
 }
 
